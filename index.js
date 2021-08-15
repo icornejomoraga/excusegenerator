877a7b8834c3b2075b257f17d19b25ef0c447f87
Isaac <icornejomoraga@gmail.com>
2021-08-15 15:22:07 -0400

    first commit

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-window.onload = function() {
+window.onload = () => {
 
     //let randomnumber = Math.random () * 10;
     document.querySelector("#excuse").innerHTML = generateexcuse();
-    console.log('hello'+randomnumber);
+    console.log('hello');
            
-}
+};
 
 let generateexcuse = () => {
 
   
   let pronoun = ['An','The'];
   let someone = ['alien', 'bear', 'ninja'];
-  let action = ['stole','destroyed',];
+  let action = ['stole','destroyed'];
   let something = ['my homework', 'my wallet', 'my bike'];
   let where = ['outside my house', 'on the street', 'at the supermarket']; 
   
-  let proindex = math.floor(Math.random() * pronoun.length);
-  let somindex = 0;
-  let actionindex = 0;  
-  let somethingindex = 0;  
-  let whereindex = 0;
+  
 
-  //let proindex = math.floor(math.randomnumber() * pronoun.length);
-  //let somindex = math.floor(math.randomnumber() * someone.length);
-  //let actionindex = math.floor(math.randomnumber() * action.length);
-  //let somethingindex = math.floor(math.randomnumber() * something.length);
-  //let whereindex = math.floor(math.randomnumber() * where.length);
+  let proindex = math.floor(Math.random() * pronoun.length);
+  let somindex = math.floor(Math.random() * someone.length);
+  let actionindex = math.floor(Math.random() * action.length);
+  let somethingindex = math.floor(Math.random() * something.length);
+  let whereindex = math.floor(Math.random() * where.length);
 
 
 
